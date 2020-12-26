f19614c1c653c0da14600f109389378d92a9108c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-12-26 23:57:44 +0200

    Updated-2.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,18 +7,47 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String fileName1=  "KUKU1.txt";
-        String fileName2=  "KUKU2.txt";
+        String fileName1 = "KUKU1.txt";
+        String fileName2 = "KUKU2.txt";
         File file1 = new File(fileName1);
         File file2 = new File(fileName2);
         if(file1.exists() || file2.exists())
         {
-            System.out.println("+");
+            try
+                    (
+                            FileInputStream fo1 = new FileInputStream(fileName1);
+                            FileInputStream fo2 = new FileInputStream(fileName2);
+                    )
+            {
+                System.out.println("+");
+
+                byte[] buffer1 = new byte[fo1.available()];
+                fo1.read(buffer1);
+                String data1 = new String(buffer1);
+                System.out.println(data1);
+
+                byte[] buffer2 = new byte[fo2.available()];
+                fo2.read(buffer2);
+                String data2 = new String(buffer2);
+                System.out.println(data2);
+
+                if (data1.indexOf(data2) != -1)
+                {
+                    System.out.println("+");
+                }
+                else
+                {
+                    System.out.println("-");
+                }
+            }
+            catch (IOException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
         }
         else
         {
             System.out.println("-");
         }
-
     }
 }
